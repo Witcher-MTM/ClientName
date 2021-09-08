fc3737d8bf3bdead90d0d2e7c34ae81251a2d9d7
Witcher-MTM <phantomit228@gmail.com>
2021-09-08 11:25:33 +0300

    Init commit

--- ClientServer/Program.cs
+++ ClientServer/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ClientServer
 {
     class Program
     {
+        static int port = 8000;
         static void Main(string[] args)
         {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Bind(iPEndPoint);
+                socket.Listen(10);
+
+                Console.WriteLine("Start server...");
+
+                while (true)
+                {
+                    Socket socketClient = socket.Accept();
+                    StringBuilder stringBuilder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    do
+                    {
+                        bytes = socketClient.Receive(data);
+                        stringBuilder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (socketClient.Available > 0);
+
+                    Console.WriteLine($"MSG: {stringBuilder.ToString()}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.WriteLine("Hello World!");
+          
         }
     }
 }
